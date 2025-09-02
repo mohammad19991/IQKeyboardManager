d94e5545b54308d88466a646f894e4eb2ed62182
Mohammad Alatrash <mkalatrash@gmail.com>
2025-09-02 11:53:41 +0300

    update package.swift

--- Package.swift
+++ Package.swift
@@ -15,10 +15,10 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/hackiftekhar/IQKeyboardNotification.git", from: "1.0.5"),
-        .package(url: "https://github.com/hackiftekhar/IQTextInputViewNotification.git", from: "1.0.8"),
-        .package(url: "https://github.com/hackiftekhar/IQKeyboardReturnManager.git", from: "1.0.5"),
-        .package(url: "https://github.com/hackiftekhar/IQTextView.git", from: "1.0.5"),
-        .package(url: "https://github.com/mohammad19991/IQKeyboardToolbarManager.git", from: "1.1.5"),
+        .package(url: "https://github.com/mohammad19991/IQTextInputViewNotification.git", from: "1.0.9"),
+        .package(url: "https://github.com/mohammad19991/IQKeyboardReturnManager.git", from: "1.0.7"),
+        .package(url: "https://github.com/mohammad19991/IQTextView.git", from: "1.0.6"),
+        .package(url: "https://github.com/mohammad19991/IQKeyboardToolbarManager.git", from: "1.1.7"),
     ],
     targets: [
         .target(name: "IQKeyboardManagerSwift",
